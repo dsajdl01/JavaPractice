c4e25f221831f2b47910d5f0466e61dbb78d4596
dsajdl01 <davidsajdl@yahoo.co.uk>
2015-08-06 22:59:19 +0100

    adding class LondonEthnicGroup

---
+++ ProgramUsingHashMap/src/programUsingHashMap/LondonEthnicGroup.java
@@ -0,0 +1,84 @@
+package programUsingHashMap;
+
+public class LondonEthnicGroup {
+	String borough;
+	int allPeople, whiBrith, irish, otherWhite, WhiBlacCar, whiBlackAff, whitAsia, otheMix;
+	int india, pakis, bang, otherAsia, blackCar, afrBlack, otherBlack, chinese, otherEtnicGroup;
+	
+	public LondonEthnicGroup(String bor,int all, int br, int ir, int otWH, int wb, int wba,int wa,int om, int i, int p, int b, int oa, int bc, int ab, int ob, int ch, int oeg){
+		this.borough = bor;
+		this.allPeople = all;
+		this.whiBrith = br;
+		this.india = ir;
+		this.otherWhite = otWH;
+		this.WhiBlacCar =  wb;
+		this.whiBlackAff = wba;
+		this.whitAsia = wa;
+		this.otheMix = om;
+		this.india = i;
+		this.pakis = p;
+		this.bang = b;
+		this.otherAsia = oa;
+		this.blackCar = bc;
+		this.afrBlack = ab;
+		this.otherBlack = ob;
+		this.chinese = ch;
+		this.otherEtnicGroup = oeg;
+	}
+	public String getBorough(){
+		return this.borough;
+	}
+	
+	public int getAllPeople(){
+		return this.allPeople;
+	}
+	public int getWhiteBrithish(){
+		return this.whiBrith;
+	}
+	
+	public int getWhiteIrish(){
+		return this.irish;
+	}
+	public int getOtherWhite(){
+		return this.otherWhite;
+	}
+	public int WhiteBlackCaribbean(){
+		return this.WhiBlacCar;
+	}
+	public int getWhiteBlackAfrican(){
+		return this.whiBlackAff;
+	}
+	public int getWhiteAsian(){
+		return this.whitAsia;
+	}
+	public int getOtherMixed(){
+		return this.otheMix;
+	}
+	public int getIndian(){
+		return this.india;
+	}
+	public int getPakistani(){
+		return this.pakis;
+	}
+	public int getBangladeshi(){
+		return this.bang;
+	}
+	public int getOtherAsian(){
+		return this.otherAsia;
+	}
+	public int getCaribbeanBlackBrithish(){
+		return this.blackCar;
+	}
+	public int getAfricanBlackBritish(){
+		return this.afrBlack;
+	}
+	public int getOtherBlackBritish(){
+		return this.otherBlack;
+	}
+	public int getChinese(){
+		return this.chinese;
+	}
+	public int getOtherEthicGroup(){
+		return this.otherEtnicGroup;
+	}
+}
