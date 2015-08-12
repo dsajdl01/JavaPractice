5eb01a8e704f64cfeff6a08fd84290fa475699d2
dsajdl01 <davidsajdl@yahoo.co.uk>
2015-08-12 19:02:53 +0100

    Adding interface classes

---
+++ ProgramUsingThreadSafe/Event.java
@@ -0,0 +1,10 @@
+package tests.task2;
+
+import java.util.*;
+
+public interface Event {
+
+	public String getEventId();
+	public String getEventType();
+	public Calendar getEventTimestemp();
+}
---
+++ ProgramUsingThreadSafe/EventConsumer.java
@@ -0,0 +1,7 @@
+package tests.task2;
+
+public interface EventConsumer {
+	
+	public void consumeEvent(Event theEvent);
+	
+}
