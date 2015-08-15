1db5dd0014a53d794f7954fac1a67f1c1cfe7ebf
dsajdl01 <davidsajdl@yahoo.co.uk>
2015-08-15 14:33:58 +0100

    adding UnknownTypeException class

---
+++ ProgramUsingThreadSafe/UnknownTypeException.java
@@ -0,0 +1,10 @@
+package tests.task2;
+
+@SuppressWarnings("serial")
+public class UnknownTypeException extends Exception{
+		
+	public UnknownTypeException(String message){
+		super(message);
+	}
+
+}
