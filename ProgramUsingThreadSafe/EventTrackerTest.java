8f9b8944862025c92f7c5e5d2a98f0edd46b63c3
dsajdl01 <davidsajdl@yahoo.co.uk>
2015-08-15 14:37:23 +0100

    modified EventTrackerTest Unit test class

--- ProgramUsingThreadSafe/EventTrackerTest.java
+++ ProgramUsingThreadSafe/EventTrackerTest.java
@@ -16,6 +16,7 @@ import org.junit.Test;
 public class EventTrackerTest {
 
 	 private String[] t = {"INITIAL", "CREATING", "CREATED", "CLOSING", "CLOSED"};
+	 private String type = "INITIALS";
 	 private EventTracker et;
 	 private Events ev;
 	 private TheLocker lock;
@@ -39,15 +40,15 @@ public class EventTrackerTest {
 		this.ev = null;
 		this.lock = null;
 	}
-
+	// testin getStoreCount method when the object in created the value is 5
 	@Test
 	public void test_get_StoresCount() {
 		assertEquals("Wrong Answer",5, et.getStoreCount());
 		
 	}
-	
-	@Test
-	public void test_addEvent() throws InterruptedException{
+	// testing add event method by adding events in mix order
+    @Test
+	public void test_addEvent() throws InterruptedException, UnknownTypeException{
 		et.addEvent(ev, lock);
 		assertNotNull( et.getNextSendableEvent(lock));
 		assertEquals("Wrong Answer",4, et.getStoreCount());
@@ -72,9 +73,10 @@ public class EventTrackerTest {
 		this.loopHelper(ev, lock, et);
 		assertEquals("Wrong Answer",0, et.getStoreCount());
 	}
-	
+ 
+    // testing add event method by adding events in ascending order
 	@Test
-	public void test_addEventTwo() throws InterruptedException{
+	public void test_addEventTwo() throws InterruptedException, UnknownTypeException{
 		Events event = new Events(t[0]);
 		EventTracker evtr = new EventTracker();
 		evtr.setFirstAsSent(event, lock);
@@ -102,38 +104,49 @@ public class EventTrackerTest {
 		assertEquals("Wrong Answer",0, evtr.getStoreCount());
 		
 	}
-	
+	// testing add event method by adding events in descending order
 	@Test
-	public void test_AddEventThree() throws InterruptedException{
-		Events event = new Events(t[4]);
-		EventTracker etr = new EventTracker();
-		etr.addEvent(event, lock);
-		assertNull( etr.getNextSendableEvent(lock));
-		assertEquals("Wrong Answer",5, etr.getStoreCount());
-
-		event.setType(t[3]);;
-		etr.addEvent(event, lock);
-		assertNull( etr.getNextSendableEvent(lock));
-		this.loopHelper(event, lock, etr);
-		assertEquals("Wrong Answer",5, etr.getStoreCount());
-		
-		event.setType(t[2]);;
-		etr.addEvent(event, lock);
-		assertNull( etr.getNextSendableEvent(lock));
-		this.loopHelper(event, lock, etr);
-		assertEquals("Wrong Answer",5, etr.getStoreCount());
-		
-		event.setType(t[1]);;
-		etr.addEvent(event, lock);
-		assertNull( etr.getNextSendableEvent(lock));
-		this.loopHelper(event, lock, etr);
-		assertEquals("Wrong Answer",5, etr.getStoreCount());
-		
-		event.setType(t[0]);;
-		etr.addEvent(event, lock);
-		assertNotNull( etr.getNextSendableEvent(lock));
-		this.loopHelper(event, lock, etr);
-		assertEquals("Wrong Answer",0, etr.getStoreCount());
+	public void test_AddEventThree() throws InterruptedException, UnknownTypeException{
+			Events event = new Events(t[4]);
+			EventTracker etr = new EventTracker();
+			etr.addEvent(event, lock);
+			assertNull( etr.getNextSendableEvent(lock));
+			assertEquals("Wrong Answer",5, etr.getStoreCount());
+	
+			event.setType(t[3]);;
+			etr.addEvent(event, lock);
+			assertNull( etr.getNextSendableEvent(lock));
+			this.loopHelper(event, lock, etr);
+			assertEquals("Wrong Answer",5, etr.getStoreCount());
+			
+			event.setType(t[2]);;
+			etr.addEvent(event, lock);
+			assertNull( etr.getNextSendableEvent(lock));
+			this.loopHelper(event, lock, etr);
+			assertEquals("Wrong Answer",5, etr.getStoreCount());
+			
+			event.setType(t[1]);;
+			etr.addEvent(event, lock);
+			assertNull( etr.getNextSendableEvent(lock));
+			this.loopHelper(event, lock, etr);
+			assertEquals("Wrong Answer",5, etr.getStoreCount());
+			event.setType(t[0]);;
+			etr.addEvent(event, lock);
+			assertNotNull( etr.getNextSendableEvent(lock));
+			this.loopHelper(event, lock, etr);
+			assertEquals("Wrong Answer",0, etr.getStoreCount());
+			
+	}
+	// testing add event method when the method throw exception 
+	// by adding invalid event type 
+	@Test
+	public void test_addEvent_throwExceptionTest() throws InterruptedException{
+		try{
+			ev.setType(type);
+			et.addEvent(ev, lock);
+		} catch (UnknownTypeException ute){
+			assertEquals("Wrong Answer", "tests.task2.UnknownTypeException: Unknown event type: " + type, ute.toString());
+		}
 	}
 
 	public void loopHelper(Event e, TheLocker l, EventTracker et) throws InterruptedException{
