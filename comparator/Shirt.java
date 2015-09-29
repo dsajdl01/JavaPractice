804ec08f6c134ef2c85afaa792eb4a3397135cee
dsajdl01 <davidsajdl@yahoo.co.uk>
2015-09-29 17:24:57 +0100

    adding Shirt interface

---
+++ comparator/Shirt.java
@@ -0,0 +1,19 @@
+package OOP.comparator;
+
+
+/**
+ * interface Shirt.
+ * 
+ * @author David Sajdl 
+ * @version 7
+ */
+public interface Shirt
+{
+    
+    String getShirtId();
+    String getShirtColor();
+    String getSleevsSize();
+    int getCotonQual();
+    int getPrice();
+    int getStockQuantity();
+}
