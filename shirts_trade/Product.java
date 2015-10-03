4a71347ad8501883021ee834f357e23d3e380404
dsajdl01 <davidsajdl@yahoo.co.uk>
2015-10-03 17:04:24 +0100

    adding Product interface

---
+++ shirts_trade/Product.java
@@ -0,0 +1,16 @@
+package shirt_trade;
+/**
+ * Interface Product
+ * 
+ * @author David Sajdl 
+ * @version 7
+ */
+public interface Product {
+	
+	int getId(); 
+	String getShirtColor(); 
+	String getSleevesSize(); 
+	int getCotonQual(); 
+	double getPrice(); 
+	int getQuantity(); 
+}
