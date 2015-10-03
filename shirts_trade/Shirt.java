b1a2507da39487f089dd7cb637d2de3c7060bffe
dsajdl01 <davidsajdl@yahoo.co.uk>
2015-10-03 17:06:11 +0100

    adding Shirt class

---
+++ shirts_trade/Shirt.java
@@ -0,0 +1,67 @@
+package shirt_trade;
+
+public class Shirt implements Product {
+	
+	private int id; 
+	private String color; 
+	private String sleevSize; 
+	private int qual; 
+	private double price; 
+	private int quan; 
+	/**
+	 * Class Shirt
+	 * 
+	 * @author David Sajdl 
+	 * @version 7
+	 */
+	public Shirt(int id, String color, String sleevs, int qual, double price, int quan){ 
+	       this.id = id; 
+	       this.color = color; 
+	       this.sleevSize = sleevs; 
+	       this.qual = qual; 
+	       this.price = price; 
+	       this.quan = quan; 
+	} 
+
+
+	@Override
+	public int getId() {
+		// TODO Auto-generated method stub
+		return this.id;
+	}
+
+	@Override
+	public String getShirtColor() {
+		// TODO Auto-generated method stub
+		return this.color;
+	}
+
+	@Override
+	public String getSleevesSize() {
+		// TODO Auto-generated method stub
+		return this.sleevSize;
+	}
+
+	@Override
+	public int getCotonQual() {
+		// TODO Auto-generated method stub
+		return this.qual;
+	}
+
+	@Override
+	public double getPrice() {
+		// TODO Auto-generated method stub
+		return this.price;
+	}
+
+	@Override
+	public int getQuantity() {
+		// TODO Auto-generated method stub
+		return this.quan;
+	}
+	
+	public void plusQuantity(int q){
+		this.quan += q;
+	}
+
+}
