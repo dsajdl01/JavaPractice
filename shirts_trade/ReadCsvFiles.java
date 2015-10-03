396f733154c26085f3ae9f4377f393430654e6b9
dsajdl01 <davidsajdl@yahoo.co.uk>
2015-10-03 21:00:00 +0100

    adding ReadCsvFiles class

---
+++ shirts_trade/ReadCsvFiles.java
@@ -0,0 +1,30 @@
+package shirt_trade;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+/**
+ * Class ReadCsvFiles
+ * 
+ * @author David Sajdl 
+ * @version 7
+ */
+public class ReadCsvFiles {
+	
+	String content;
+	
+	public ReadCsvFiles(){
+		content = null;
+	}
+	
+	public BufferedReader readCSVFileFromWebsite(String url) throws IOException{
+		URL stockURL = new URL(url);
+		return new BufferedReader(new InputStreamReader(stockURL.openStream()));
+	}
+	
+	public BufferedReader readCSVFileFromCompoter(String pathToFile) throws IOException{
+		return new BufferedReader(new FileReader(pathToFile));
+	}
+
+}
