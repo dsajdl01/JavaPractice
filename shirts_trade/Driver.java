eadf87667079e024320ec43a78cb7ce93d5ecaa6
dsajdl01 <davidsajdl@yahoo.co.uk>
2015-10-04 17:44:30 +0100

    modified Driver class

--- shirts_trade/Driver.java
+++ shirts_trade/Driver.java
@@ -3,9 +3,10 @@ package shirt_trade;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 import database.Connection;
 
@@ -20,40 +21,67 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String URL =  "https://raw.githubusercontent.com/dsajdl01/JavaPractice/master/shirts_trade/stock.csv";
-		String line;
 		ReadCsvFiles read = new ReadCsvFiles();
 		try{
 			// read .csv file from web site
 			BufferedReader  out =read.readCSVFileFromWebsite(URL);
-			System.out.println("\tSTOCK");
+			ShirtProcess sp = new ShirtProcess();
+			HashMap<String, Shirt> stock = sp.getGroupShirtProduct(out);
 			
-			while((line = out.readLine()) != null){
-				System.out.println(line);
-			}
-			System.out.println("\n----------------------------\n");
 			
-			// read .csv file from local H drive  
+			// read .csv file from local H drive 
+			HashMap<String, Shirt> invoice = null;
 			File dir = new File("H:/aaa_school");
 			if(dir.exists()){
 					ReadCsvFiles rcf = new ReadCsvFiles();
 					String path = dir + "/invoice.csv";
 					BufferedReader br = rcf.readCSVFileFromCompoter(path);
-					System.out.println("\tINVOICE");
-					while((line = br.readLine()) != null){
-						System.out.println(line);
-					}
+					invoice = sp.getGroupShirtProduct(br);
 			}
 			
-			System.out.println("\n----------------------------\n");
 			// read data from database
+			// Connection class is in imported from database package
 			Connection conn = new Connection();
 			Statement  c = conn.getConnection();
 			String sqlQuery = "select  * from sale";
-			ResultSet rs =c.executeQuery(sqlQuery);
-			System.out.println("\tSALES");
-			while(rs.next()){
-				System.out.println(rs.getShort(1)+",  "+ rs.getString(2)+",  "+ rs.getString(3)+ ", "
-				+ rs.getShort(4) +",  "+ rs.getDouble(5) +",  "+ rs.getShort(6));
+			HashMap<String, Shirt> sales = sp.getGroupShirtsProduct( c.executeQuery(sqlQuery));
+			
+			// finding different between stock, invoice and sales 
+			StringBuffer output = new StringBuffer();
+			int diff, count = 0; 
+			double total = 0;
+			// looping through invoice and match data with stock and sales  
+			for(Entry<String, Shirt> inv : invoice.entrySet()){
+				// get key and values
+				String key = inv.getKey();
+				Shirt invoi = inv.getValue();
+				Shirt sold = sales.get(key);
+				Shirt stk = stock.get(key);
+				
+				// get different between files:
+				if(sold == null){
+					diff = (invoi.getQuantity()) - (stk.getQuantity());
+				} else {
+					 diff = (invoi.getQuantity()) - (sold.getQuantity() + stk.getQuantity());
+				}
+				//if there is different add record into string buffer
+				if(diff != 0){
+					count += diff;
+					output.append("The " + invoi.getShirtColor() +" shirt with " + invoi.getSleevesSize()+" sleeves and " + invoi.getCotonQual() +
+						"% cotton:\n");
+					output.append("Invoice:   " + invoi.getQuantity() + "\nStock:     "+ stk.getQuantity() + "\nSold:      "+ sold.getQuantity());
+					double amount = diff * invoi.getPrice();
+					output.append("\nDifferent: "+ diff + "\nAmount:    £" +String.format( "%.2f", amount) +"\n");
+					total += amount;
+				}
+			}
+			//printout the result
+			if(total !=  0){
+				System.out.println("The different between invoice, sale and stock is: " + count +"\n\nMISSING PRODUCT:");
+				System.out.println(output.toString());
+				System.out.println("Total cost: £" + String.format( "%.2f", total));
+			} else{
+				System.out.println("There is no different between invoice, sale and stock");
 			}
 			
 		} catch (IOException e){
@@ -62,16 +90,6 @@ public class Driver {
 			System.out.println(sqle.toString());
 		}
 	}
-	
-	public Shirt gerShirtObject(String line){
-		String[] array = line.split(",");
-		return new Shirt(getInt(array[0].trim()), array[1].trim(), array[2].trim(), 
-				getInt(array[3].trim()), Double.parseDouble(array[4].trim()), getInt(array[5].trim()));
-	}
-	
-	private int getInt(String n) {
-		// TODO Auto-generated method stub
-		return Integer.parseInt(n);
-	}
+
 
 }
